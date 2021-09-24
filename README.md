a1d93c13dc66b0029b83cb484f86a740f4a13df3
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-09-24 15:08:44 -0300

    :tada: todos os testes passaram

---
+++ README.md
@@ -0,0 +1,33 @@
+## Desafio 02 - Trabalhando com middlewares 
+
+- Desafio desenvolvido para trabalhar mais a fundo com middlewares no Express.
+---
+
+## Testes dos middlewares
+---
+### Para que todos os testes passem, deve-se fazer os seguntes requisitos:
+
+[x] Should be able to find user by username in header and pass it to request.user
+
+[x] Should not be able to find a non existing user by username in header
+
+[x] Should be able to let user create a new todo when is in free plan and have less than ten todos
+
+[x] Should be able to let user create a new todo when is in free plan and have less than ten todos
+
+[x] Should be able to let user create infinite new todos when is in Pro plan
+
+[x] Should be able to put user and todo in request when both exits
+
+[x] Should not be able to put user and todo in request when both exits
+
+[x]  Should not be able to put user and todo in request when todo id is not uuid
+
+[x] Should not be able to put user and todo in request when todo does not exists
+
+[x] Should be able to find user by id route param and pass it to request.user
+
+[x] Should not be able to pass user to request.user when it does not exists
+
+    
+
--- src/index.js
+++ src/index.js
@@ -10,20 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const  {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error:"User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  const todoCounter = user.todos.length >= 10 ? true:false;
+
+  if(!user.pro && todoCounter) {
+    response.status(403).json({error:"User out of free plan or reached 10 all!"});
+  }
+
+  if(user.pro || !todoCounter) {
+    return next();
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user){
+   return  response.status(404).json({error:"User not found!"});
+  }
+
+  
+  if (!validate(id)){
+    return response.status(400).json({error:"Invalid uuid informed!"});
+  }
+ 
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo){
+     return response.status(404).json({error:"Id does not belong to task"});
+  }
+
+  request.todo = todo;
+  
+  request.user = user;
+
+
+  return next();  
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  
+  const user = users.find((user) => user.id === id);
+
+  if(!user){
+     return response.status(404).json({error:"User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
+
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
